eef046ef344903f8555826a0400768db8d12e31f
Felipe Vicente <vcnte.felipe@outlook.com>
2024-02-26 19:16:35 -0300

    GUID added in Entity class

--- ReviewOOP/SharedContext/Entity.cs
+++ ReviewOOP/SharedContext/Entity.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace ReviewOOP.SharedContext
 {
-    public class Entity
+    public abstract class Entity
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Url { get; set; } = string.Empty;
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
     }
 }
