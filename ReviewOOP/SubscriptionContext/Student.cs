abd9fa59ec074a799c68439ca4392c491d1d6937
Felipe Vicente <vcnte.felipe@outlook.com>
2024-02-27 19:45:17 -0300

    student and user class created

---
+++ ReviewOOP/SubscriptionContext/Student.cs
@@ -0,0 +1,16 @@
+﻿using ReviewOOP.SharedContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewOOP.SubscriptionContext
+{
+    public class Student : Entity
+    {
+        public User User { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
---
+++ ReviewOOP/SubscriptionContext/User.cs
@@ -0,0 +1,15 @@
+﻿using ReviewOOP.SharedContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewOOP.SubscriptionContext
+{
+    public class User : Entity
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
