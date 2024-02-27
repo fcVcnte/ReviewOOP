c2d4d09a0bc5ebe1b31d6a03baebf82826a4b49d
Felipe Vicente <vcnte.felipe@outlook.com>
2024-02-27 19:25:58 -0300

    notification sealed class was created

---
+++ ReviewOOP/NotificationContext/Notification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewOOP.NotificationContext
+{
+    public sealed class Notification
+    {
+        public Notification()
+        {
+            
+        }
+        public Notification(string property, string message)
+        {
+            Property = property;
+            Message = message;
+        }
+        public string Property { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
