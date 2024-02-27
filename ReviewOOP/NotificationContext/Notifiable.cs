4e98ae0e918c04b786380139cf15dba9907cb88d
Felipe Vicente <vcnte.felipe@outlook.com>
2024-02-27 19:41:37 -0300

    constructor created in notifiable abstract class

--- ReviewOOP/NotificationContext/Notifiable.cs
+++ ReviewOOP/NotificationContext/Notifiable.cs
@@ -9,14 +9,20 @@ namespace ReviewOOP.NotificationContext
     public abstract class Notifiable
     {
         public List<Notification> Notifications { get; set; }
+        public bool IsInvalid => Notifications.Any();
 
-        public void Add(Notification notification)
+        public void AddNotification(Notification notification)
         {
             Notifications.Add(notification);
         }
-        public void AddRange(IEnumerable<Notification> notifications)
+        public void AddNotifications(IEnumerable<Notification> notifications)
         {
             Notifications.AddRange(notifications);
+        }
+
+        public Notifiable()
+        {
+            Notifications = new List<Notification>();
         }
     }
 }
